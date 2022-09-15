26051313ec3fb25579db150efbd50066657f75f5
DishantPal <97158832+DishantPal@users.noreply.github.com>
2022-09-15 18:29:42 +0530

    Type fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
         <img class="d-block mx-auto mb-4" src="https://raw.githubusercontent.com/VanOns/laraberg/main/logo-text.svg" alt="" width="72" height="57">
         <h1 class="display-5 fw-bold">Laraberg Demo</h1>
         <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">Laraberg brings the power of wordpress guttenber to laravel</p>
+          <p class="lead mb-4">Laraberg brings the power of wordpress guttenber to laravel.</p>
           <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <a href="{{route('posts.index')}}" class="btn btn-primary btn-lg px-4 gap-3">View Posts</a>
             <a href="{{route('posts.create')}}" class="btn btn-outline-secondary btn-lg px-4">Create Post</a>
